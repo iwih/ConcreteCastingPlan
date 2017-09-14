6a99546f47a98cc8b57f3317e4485dea8f2bef8c
iwih <osama.a.shammari@gmail.com>
2017-09-14 19:09:42 +0300

    EditMouldsActivity: Used drawable selector for the background.

--- app/src/main/java/com/iwih/concretecastingplan/EditMouldsActivity.java
+++ app/src/main/java/com/iwih/concretecastingplan/EditMouldsActivity.java
@@ -1,12 +1,8 @@
 package com.iwih.concretecastingplan;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 
 public class EditMouldsActivity extends AppCompatActivity {
 
@@ -17,34 +13,6 @@ public class EditMouldsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        setColorChangingListener();
-    }
-
-    private void setColorChangingListener() {
-        Button addMouldBtn = (Button) findViewById(R.id.edit_moulds_add_new);
-        Button removeMouldBtn = (Button) findViewById(R.id.edit_moulds_remove_selected);
-
-        View.OnTouchListener onTouchListener = colorChangingListener();
-        addMouldBtn.setOnTouchListener(onTouchListener);
-        removeMouldBtn.setOnTouchListener(onTouchListener);
-    }
-
-    @NonNull
-    private View.OnTouchListener colorChangingListener() {
-        return new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        v.setBackgroundColor(getResources().getColor(R.color.button_down));
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        v.setBackgroundColor(getResources().getColor(R.color.button_normal));
-                        break;
-                }
-                return false;
-            }
-        };
     }
 
 }
